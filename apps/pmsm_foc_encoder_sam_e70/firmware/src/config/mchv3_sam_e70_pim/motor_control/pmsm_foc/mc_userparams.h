08814cd983fd515864fe6700852ee35275e507c6
Utsav Somani <Utsav.Somani@microchip.com>
2020-12-02 23:46:28 -0700

    [DFT] Regenerate and DFT of pmsm_foc_encoder_sam_e70 (mchv3) for v3.6.0 release MH3-45888

--- apps/pmsm_foc_encoder_sam_e70/firmware/src/config/mchv3_sam_e70_pim/motor_control/pmsm_foc/mc_userparams.h
+++ apps/pmsm_foc_encoder_sam_e70/firmware/src/config/mchv3_sam_e70_pim/motor_control/pmsm_foc/mc_userparams.h
@@ -42,6 +42,7 @@
 #ifndef _USER_HEADER
 #define _USER_HEADER
 
+#include "mc_pmsm_foc_common.h"
 /***********************************************************************************************/
 /*                    include files                                                            */
 /***********************************************************************************************/
@@ -60,8 +61,8 @@
 #define FIELD_WEAKENING                  (DISABLED)  /* If enabled - Field weakening */
 #define ALIGNMENT_METHOD                 (FORCED_ALIGNMENT)  /* alignment method  */
 
-#define Q_AXIS_ALIGNMENT                 (ENABLED)
 
+#define Q_AXIS_ALIGNMENT                 (ENABLED)
 #define CURRENT_MEASUREMENT              (DUAL_SHUNT)  /* Current measurement shunts */
 
 #define POTENTIOMETER_INPUT_ENABLED       ENABLED
@@ -95,7 +96,7 @@
 
 /* Field weakening - Limit for -ve Idref */
 #if(FIELD_WEAKENING == ENABLED)
-#define MAX_FW_NEGATIVE_ID_REF              (float)(-2)
+#define MAX_FW_NEGATIVE_ID_REF              (float)(-2.5)
 #endif
 
 /******************************************************************************/
@@ -115,7 +116,7 @@
 
 /******* Velocity Control Loop Coefficients **********************************/
 #define     SPEEDCNTR_PTERM            (float)(0.005)
-#define     SPEEDCNTR_ITERM            (float)(0.00002 * 0.01f)
+#define     SPEEDCNTR_ITERM            (float)(0.000002 * 0.01f)
 #define     SPEEDCNTR_CTERM            (float)(0.5)
 #define     SPEEDCNTR_OUTMAX           (float)(4)
 
